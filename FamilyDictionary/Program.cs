c9e8737ec8e031191f1e4869bd50fdd608fb9ecd
Austin <austincumberlander@gmail.com>
2019-03-15 17:44:45 -0500

    done

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FamilyDictionary
 {
@@ -6,7 +7,20 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+            myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Krista" }, { "age", "42" } });
+
+
+
+            foreach(var member in myFamily)
+            {
+                var name = member.Value["name"];
+                var sister = member.Key;
+                var age = member.Value["age"];
+
+                Console.WriteLine($"{name} is my {sister} and is {age} years old.");
+                Console.ReadLine();
+            }
         }
     }
 }
